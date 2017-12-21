780722ac5134221158e1f005436b746607f91c8f
Adrian <adrianslolacc@gmail.com>
2017-12-21 15:32:14 -0600

    added fullscreen function to video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('[data-full]');
 
 /* build out functions */
 function togglePlay() {
@@ -39,6 +40,10 @@ function scrub(e) {
     video.currentTime = scrubTime;
 }
 
+function toggleScreen() {
+    player.classList.toggle('fullscreen')
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -52,6 +57,13 @@ skipButtons.forEach(skipButton => {
 });
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
 progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+fullScreen.addEventListener('click', toggleScreen);
 
 
